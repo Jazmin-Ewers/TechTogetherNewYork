58c541d3759699edaea4d448d1f8e9c83ad58d34
jaz_ewe <jazminewers11@gmail.com>
2021-10-15 13:22:58 -0400

    Initial Commit

---
+++ TechTogetherNewYork/TechTogetherNewYorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechTogetherNewYorkApp.swift
+//  TechTogetherNewYork
+//
+//  Created by Computer on 10/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TechTogetherNewYorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
